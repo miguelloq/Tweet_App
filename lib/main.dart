dbf4c2f9ee3ed361090d05a036680e924c007a39
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2023-07-04 17:46:43 -0300

    Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tweet_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  firestore
+      .collection('First collection')
+      .doc('Hello Firestore')
+      .set({'output': true});
 }
 
 class MyApp extends StatelessWidget {
