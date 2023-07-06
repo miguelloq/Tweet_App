6b20a550a684a4c474baa57f25db5511304fe81e
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2023-07-06 19:47:19 -0300

    Initial AppWidget with StreamBuilder for Auth

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'features/auth/login/login_screen.dart';
+import 'features/home/home_screen.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -9,9 +13,26 @@ class AppWidget extends StatelessWidget {
       title: 'Tweet App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        brightness: Brightness.dark,
+        primarySwatch: Colors.indigo,
+        primaryColor: Colors.indigo,
+        brightness: Brightness.light,
       ),
-      home: const Placeholder(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.userChanges(),
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (snapshot.hasData) {
+              return const HomeScreen();
+            } else {
+              return const LoginScreen();
+            }
+          }
+        }),
+      ),
     );
   }
 }
